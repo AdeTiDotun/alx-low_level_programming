d7bf1eed8c13708714ec944b84da98ca649846b9
Daniel <adejumodaniel274@gmail.com>
2023-02-16 14:00:17 -0800

    size of char

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 /**
- * main: size of types
+ * main: c-program that prints the size of computer types
  * Return: 0
  */
 #include <stdio.h>
@@ -7,9 +7,8 @@ int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-	printf("Size of a long int: %ld byte(s)", sizeof(long int));
-	printf("Size of a long long int: %ld byte(s)", sizeof(long long int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)", sizeof (float));
-
 	return (0);
 }
