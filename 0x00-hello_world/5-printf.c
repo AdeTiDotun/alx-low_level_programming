2fcf052a8459fcb0a3eddb3f83f896080d7ea379
Daniel <adejumodaniel274@gmail.com>
2023-02-16 13:20:48 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main- using prinf to print a message
+ * Return: 0
+ */
+#include <stdio.h>
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
