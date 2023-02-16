07afe9ed44af34f6060103119bf31844a1a17769
Daniel <adejumodaniel274@gmail.com>
2023-02-16 12:12:10 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
+/**
+ * main- Aprogram thats uses puts to print a string
+ * Return: ends with 0
+ */
 #include <stdio.h>
-
-int main (void)
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n");
-
+	puts ("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
