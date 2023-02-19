a67f916a4fd395c2d0ed3a46ab284ee6c01d6300
Daniel <adejumodaniel274@gmail.com>
2023-02-19 13:04:04 -0800

    digits combined

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0 ; digit1 < 10 ; digit1++)
+	for (digit1 = '0' ; digit1 < '10' ; digit1++)
 	{
-		for (digit2 = 0 ; digit2 < 10 ; digit2++)
+		for (digit2 = "0" ; digit2 < "10" ; digit2++)
 		{
 			putchar(digit1);
 			putchar(digit2);
