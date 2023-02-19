227230075d3e4c7a2996ef8c75c990ba5c75c4b9
Daniel <adejumodaniel274@gmail.com>
2023-02-19 07:36:38 -0800

    numbers from 0-9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- This program prints numbers 0 - 9, using the putchar function.
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
