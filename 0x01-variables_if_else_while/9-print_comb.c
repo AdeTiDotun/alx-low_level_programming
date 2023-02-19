c1febd589a0780520b31c33fdae94a7302ae3138
Daniel <adejumodaniel274@gmail.com>
2023-02-19 09:34:01 -0800

    0-9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,9 +14,12 @@ int main(void)
 	for (num = '0' ; num <= '9' ; num++)
 	{
 		putchar(num);
-		if (num < 9)
 
-		putchar(', ');
+		if (num < 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
 
 	putchar('\n');
