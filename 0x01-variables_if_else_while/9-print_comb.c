2645945e6ded121b1f8b08522220860798b23ded
Daniel <adejumodaniel274@gmail.com>
2023-02-19 09:01:36 -0800

    0-9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main- Prints single number 0 - 9
  * Return: Alwyas (0) Success
@@ -9,11 +12,12 @@ int main(void)
 	int num;
 
 	for (num = '0' ; num <= '9' ; num++)
+	{
 		putchar(num);
 
-	if (num == 9);
-		continue;
-		putchar(',')
+		putchar(' ');
+		putchar(',');
+	}
 
 	putchar('\n');
 
