e630d7b8156ac407e05246687922c8c88d9fc242
Daniel <adejumodaniel274@gmail.com>
2023-02-19 08:25:20 -0800

    print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0 ; num <=9 ; num++)
+	for (num = '0' ; num <= '9' ; num++)
 		putchar(num);
-	for (letter = 'A' ; letter <= 'F' ; letter++)
+	for (letter = 'a' ; letter <= 'f' ; letter++)
 		putchar(letter);
 
 	putchar('\n');
