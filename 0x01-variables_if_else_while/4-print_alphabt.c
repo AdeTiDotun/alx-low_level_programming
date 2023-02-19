3c34fe72fd35feef52c8810a5a07ea68f97c3ea5
Daniel <adejumodaniel274@gmail.com>
2023-02-19 06:37:24 -0800

    alphabets without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- This program prints alphabets in lower case, with exemption to q and e
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a' ; l <= 'z' ; l++)
+	{
+		if (l != 'e' && l != 'q')
+			putchar(l);
+	}
+
+	putchar('10');
+	return (0);
+}
