dc75d3e0121ce5b42373db7b378563984671141e
Daniel <adejumodaniel274@gmail.com>
2023-02-20 13:10:12 -0800

    digits combined

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,21 +9,24 @@
 
 int main(void)
 {
-	int d, p, q;
+	int d, p,q;
 
-	for (d = '0'; d < '9'; d++)
+       	for (d = '0'; d < '9'; d++)
 	{
-		for (q = p + 1; q <= '9'; q++)
+		for (p = d + 1; p <= '9'; p++)
 		{
-			if ((p != d) != q)
+			for (q = p + 1; q <= '9'; q++)
 			{
+				if ((p != d) != q)
+				{
 				putchar(d);
 				putchar(p);
 				putchar(q);
 				if (d == '7' && p == '8')
-					continue;
+				continue;
 				putchar(',');
 				putchar(' ');
+				}
 			}
 		}
 	}
