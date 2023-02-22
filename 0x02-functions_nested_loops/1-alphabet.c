77012d999dc1342ca58af464a32d8f5f7e7013ce
Daniel <adejumodaniel274@gmail.com>
2023-02-21 21:58:43 -0800

    is lower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet - print the alphabets
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int n;
+	char lowercase;
+
+	for (n = 1, lowercase = 'a'; n <= 26; n++, lowercase++)
+	{
+		_putchar(lowercase);
+	}
+	_putchar('\n');
+}
