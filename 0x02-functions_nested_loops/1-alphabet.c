b6fe6eb88d325e9c28c292a065f4bed232898059
Daniel <adejumodaniel274@gmail.com>
2023-02-22 05:30:04 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
+#include "main.h"
 /**
- * print_alphabet - print the alphabets
- * followed by a new line
- * Return: Always 0 (Success)
- */
-
+* print_alphabet - print the alphabets
+* followe by a new line
+* Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 	int n;
