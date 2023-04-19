69153255d1098b47746998518e8b760de8d80210
daniel <adejumodaniel27@gmail.com>
2023-04-19 07:40:11 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -10,18 +11,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	if  (b == NULL)
-		return (0);
-	size_t len = strlen(b);
-	unsigned int total = 0;
+	unsigned int total, power;
+	int len;
 
-	for (size_t i = o; i < len; i++)
+	if (b == NULL)
+		return (0);
+
+	for (len = 0; b[len]; len++)
 	{
-		char c = b[i];
-	if (c != '0' && c != '1')
-		return (0);
-
-	total = (total << 1) + (c - '0');
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 	}
+
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
 	return (total);
 }
